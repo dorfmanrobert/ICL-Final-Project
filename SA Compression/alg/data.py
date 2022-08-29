35ed76551b0e47742c2a80b9fa5853b5cf26cd4a
dorfmanrobert <108150810+dorfmanrobert@users.noreply.github.com>
2022-08-29 15:04:32 +0100

    Update data.py

--- SA Compression/alg/data.py
+++ SA Compression/alg/data.py
@@ -6,8 +6,63 @@ import numpy as np
 import medmnist
 from medmnist.dataset import BreastMNIST
 import pickle
+class SyntheticDataset(Dataset):
+    def __init__(self, n=1000):
+        dataset_init = sklearn.datasets.make_classification(n_samples=n, n_features=2, n_informative=2, n_redundant=0, n_repeated=0, n_classes=2, n_clusters_per_class=2, weights=None, flip_y=0.01, class_sep=1.0, hypercube=True, shift=0.0, scale=1.0, shuffle=True, random_state=None)
+        self.data = torch.Tensor(dataset_init[0])
+        self.targets = torch.Tensor(dataset_init[1])
+        self.dataset = []
+        for i in range(len(self.data)):
+            datapoint = (self.data[i], int(self.targets[i].item()))
+            self.dataset.append(datapoint)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        return self.dataset[idx]
+
+    
+def loaddataset(name_data):
+    if name_data == 'binarymnist':
+        
+        # Load MNIST data
+        transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+        train = datasets.MNIST(
+                    'mnist-data/', train=True, download=True, transform=transform)
+        test = datasets.MNIST(
+                    'mnist-data/', train=False, download=True, transform=transform)
+        # Binarize
+        train.targets = (train.targets > 4).float()
+        test.targets = (test.targets > 4).float()
+ 
+    elif name_data == 'medmnist':
+        data_transform = transforms.Compose(
+            [transforms.ToTensor(),
+            transforms.Normalize(mean=[.5], std=[.5])]
+        )
+
+        # Load MedMNIST data
+        download = True
+        input_root = 'data'
+        train_short = BreastMNIST(root=input_root, split='train', transform=data_transform, download=download)
+        val = BreastMNIST(root=input_root, split='val', transform=data_transform, download=download)
+        train = ConcatDataset([train_short, val])
+        test = BreastMNIST(root=input_root, split='test', transform=data_transform, download=download)
+
+    
+    elif name_data == 'synthetic':
+        n_train = 100
+        n_test = 50000
+        train = SyntheticDataset(n=n_train)
+        test = SyntheticDataset(n=n_test)
+
+    return train, test
 
 
+# The below is adapted from https://github.com/mperezortiz/PBB
 def loadbatches(train, test, loader_kargs, batch_size=100, perc_train=1.0, num_supp_init=5, continuing=True):
     """Function to load the batches for the dataset
 
@@ -66,34 +121,3 @@ def loadbatches(train, test, loader_kargs, batch_size=100, perc_train=1.0, num_s
 
     return supp_loader, nonsupp_loader, supp_indx, nonsupp_indx, train_loader, test_loader
 
-
-
-def loaddataset(name_data):
-    if name_data == 'binarymnist':
-        
-        # Load MNIST data
-        transform = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
-        )
-        train = datasets.MNIST(
-                    'mnist-data/', train=True, download=True, transform=transform)
-        test = datasets.MNIST(
-                    'mnist-data/', train=False, download=True, transform=transform)
-        # Binarize
-        train.targets = (train.targets > 4).float()
-        test.targets = (test.targets > 4).float()
-
-    elif name_data == 'medmnist':
-        data_transform = transforms.Compose(
-            [transforms.ToTensor(),
-            transforms.Normalize(mean=[.5], std=[.5])]
-        )
-
-        # Load the data
-        download = True
-        input_root = 'data'
-        train = BreastMNIST(root=input_root, split='train', transform=data_transform, download=download)
-        test = BreastMNIST(root=input_root, split='test', transform=data_transform, download=download)
-
-
-    return train, test
